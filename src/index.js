0c7a07a0d6bb0d3a212055cc8ddb5593688c0336
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-02-20 16:07:37 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+      return 0;
+  } else {
+    let min = 1000;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+        } else {
+            min = min
+        }
+    }
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let max = -1000;
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            } else {
+                max = max
+            }
+        }
+        return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
 }
